4306fd18800835dc56a922802d753ad46a2e0007
Kishoren1 <kishorenaskar1@gmail.com>
2022-01-09 21:28:34 +0530

    all updated

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,14 +1,14 @@
+import { graphql } from "gatsby"
 import React from "react"
 import { getImage } from "gatsby-plugin-image"
 import Background from "../components/share/Background"
 import Layout from "../components/share/Layout"
-import { graphql, Link } from "gatsby"
 import Seo from "../components/share/Seo"
 
-export default function Error({ data }) {
-  const error = data.allContentfulPrivacyPage.nodes[0]
+export default function thanks({ data }) {
+  const thanks = data.allContentfulPrivacyPage.nodes[0]
 
-  const pluginImage = getImage(error.background.gatsbyImageData)
+  const pluginImage = getImage(thanks.background.gatsbyImageData)
 
   const bgImage = [
     `linear-gradient(rgba(233, 186, 186, 0.208), rgba(20, 20, 20, 0.479))`,
@@ -18,21 +18,17 @@ export default function Error({ data }) {
   return (
     <Layout>
       <Seo
-        title="404"
+        title="Thanks"
         description=""
         keywords="Pratham Milan Garden, Pratham Milan banquet hall"
       />
-      <Background image={bgImage} title="404" />
+      <Background image={bgImage} title="Thanks" />
 
       <div className="page-content container">
-        <h2>Opps! Invalid page</h2>
+        <h2>Thank you for your inquery</h2>
         <p>
-          Looks like you have landed in a wrong page or the page does not exist.
-          Kindly back to the &nbsp;
-          <Link to="/" style={{ textDecoration: "underline" }}>
-            Home
-          </Link>
-          &nbsp; page
+          We have received your request, and will get back to you very soon. We
+          usually response within 24 hours
         </p>
       </div>
     </Layout>
